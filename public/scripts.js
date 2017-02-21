f034eef82c592f8e778a906bd3adcd1d8705abd3
Jamie Locatis <jlocatis@gmail.com>
2017-02-21 14:04:10 -0600

    Final version of JSON_AJAX drill 3

--- public/scripts.js
+++ public/scripts.js
@@ -7,20 +7,18 @@ function getMovies() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', '/results?query=' + movie);
 	xhr.onload = function() {
-		var response = xhr.responseText;
+		response = xhr.responseText;
 		response = JSON.parse(response);
 		postMovieInfo(response);
 	}
 	xhr.send();
 }
 
-var counter = 0
-
 function postMovieInfo(response) {
 	counter = 0;
 	var remove = document.getElementById("show_me_stuff");
 	remove.innerHTML = "";
-	var html = "Title: " + response["Search"][counter]["Title"] + "<br>Year: " + response["Search"][counter]["Year"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a><br>Type: " + response["Search"][counter]["Type"] + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
+	var html = response["Search"][counter]["Title"] + "<br>" + response["Search"][counter]["Year"] + " " + response["Search"][counter]["Type"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a>" + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
 	var div = document.getElementById("show_me_stuff");
 	div.insertAdjacentHTML("afterbegin", html);
 
@@ -32,18 +30,24 @@ function postMovieInfo(response) {
 
 function previousItem(response) {
 	counter -= 1
+	if (counter < 0) {
+		counter = response["Search"].length - 1
+	}
 	var remove = document.getElementById("show_me_stuff");
 	remove.innerHTML = "";
-	var html = "Title: " + response["Search"][counter]["Title"] + "<br>Year: " + response["Search"][counter]["Year"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a><br>Type: " + response["Search"][counter]["Type"] + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
+	var html = response["Search"][counter]["Title"] + "<br>" + response["Search"][counter]["Year"] + " " + response["Search"][counter]["Type"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a>" + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
 	var div = document.getElementById("show_me_stuff");
 	div.insertAdjacentHTML("afterbegin", html);
 }
 
 function nextItem(response) {
 	counter += 1
+	if (counter == response["Search"].length) {
+		counter = 0
+	}
 	var remove = document.getElementById("show_me_stuff");
 	remove.innerHTML = "";
-	var html = "Title: " + response["Search"][counter]["Title"] + "<br>Year: " + response["Search"][counter]["Year"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a><br>Type: " + response["Search"][counter]["Type"] + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
+	var html = response["Search"][counter]["Title"] + "<br>" + response["Search"][counter]["Year"] + " " + response["Search"][counter]["Type"] + "<br>IMDB Page: <a href=\"http://www.imdb.com/title/" + response["Search"][counter]["imdbID"] + "/\" target=\"blank\">Click Here!</a>" + "<br>Poster: <br><img src=\"" + response["Search"][counter]["Poster"] + "\" alt=\"" + response["Search"][counter]["Title"] + "\">"
 	var div = document.getElementById("show_me_stuff");
 	div.insertAdjacentHTML("afterbegin", html);
 }
